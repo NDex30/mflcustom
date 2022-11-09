6a83d18615ddd5c6494cef5343569f2327872064
Ndex30 <oudex30@gmail.com>
2022-11-08 22:07:07 -0600

    oops

--- custom.js
+++ custom.js
@@ -4,7 +4,7 @@ $(function() {
 const d = new Date();
 let ms = d.getMilliseconds();
 $.get( "https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_idp_08.txt?RANDOM="+ms, function( data ) {
-    console.log('data');
+    console.log(data);
   },"text");
 // console.log($.get("https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_idp_08.txt?RANDOM="+ms));
 });
