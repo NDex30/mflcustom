f3072cb031b36759fb5491c4e3b6aee944cd322c
Ndex30 <oudex30@gmail.com>
2022-11-08 22:01:09 -0600

    trying to fetch scoring URL

--- custom.js
+++ custom.js
@@ -1,4 +1,7 @@
 $(function() {
 //  console.log(localStorage.getItem("cache_weeklyResults-3_2022_58663_1665792000"));
 //  console.log(franchiseDatabase);
+const d = new Date();
+let ms = d.getMilliseconds();
+console.log($.get("https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_idp_08.txt?RANDOM="+ms));
 });
