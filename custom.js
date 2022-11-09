fdf99386b4f7dd5120192fda1df6d409bbe5c721
Ndex30 <oudex30@gmail.com>
2022-11-08 20:54:39 -0600

    checking access to dom vars

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
 $(function() {
- console.log(localStorage.getItem("cache_weeklyResults-3_2022_58663_1665792000"))
+ console.log(localStorage.getItem("cache_weeklyResults-3_2022_58663_1665792000"));
+ console.log(franchiseDatabase);
 });
