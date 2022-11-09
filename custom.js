617a5411ed6466ee6ea4ffd5f6641f4f93732926
Ndex30 <oudex30@gmail.com>
2022-11-08 21:22:00 -0600

    comment out logs

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
 $(function() {
- console.log(localStorage.getItem("cache_weeklyResults-3_2022_58663_1665792000"));
- console.log(franchiseDatabase);
+//  console.log(localStorage.getItem("cache_weeklyResults-3_2022_58663_1665792000"));
+//  console.log(franchiseDatabase);
 });
