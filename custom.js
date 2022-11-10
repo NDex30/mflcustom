dbe830b2027e25fa7e8e9fc107d34e5aa70256c7
Ndex30 <oudex30@gmail.com>
2022-11-09 20:37:45 -0600

    flushing out

--- custom.js
+++ custom.js
@@ -2,53 +2,82 @@ $(function() {
 //  console.log(localStorage.getItem("cache_weeklyResults-3_2022_58663_1665792000"));
 //  console.log(franchiseDatabase);
     //starts at week3
-    for(i = 3;i < 4;i++) {
+    for(i = 3;i < 5;i++) {
         let formattedWeek = i.toLocaleString('en-US', {
             minimumIntegerDigits: 2,
             useGrouping: false
         })
-        var liveScoring,liveStats;
-        const d = new Date();
-        let ms = d.getMilliseconds();
-        $.ajax({
-            async: false, //thats the trick
-            url: "https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_"+formattedWeek+".txt?RANDOM="+ms,
-            dataType: 'text',
-            success: function( data ) {
-                if(console)console.log("nickdebug2",data)
-                liveStats = data
-            }
-        }).fail(function() {
-                if(console)console.log( "error" );
-        });
-        $.ajax({
-            async: false, //thats the trick
-            url: "https://www80.myfantasyleague.com/2022/export?TYPE=liveScoring&L=58663&W="+i+"&JSON=1",
-            dataType: 'json',
-            success: function( data ) {
-                if(console)console.log("nickdebug3",data)
-                liveScoring = data
-            }
-        }).fail(function() {
-                if(console)console.log( "error" );
-        });
-        // $.get( "https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_"+formattedWeek+".txt?RANDOM="+ms, function( data ) {
-        //     if(console)console.log("nickdebug2",data)
-        //     liveStats = data
-        // },"text").fail(function() {
-        //     if(console)console.log( "error" );
+        // var liveScoring,liveStats;
+        // const d = new Date();
+        // let ms = d.getMilliseconds();
+        // $.ajax({
+        //     async: false, 
+        //     url: "https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_"+formattedWeek+".txt?RANDOM="+ms,
+        //     dataType: 'text',
+        //     success: function( data ) {
+        //         // if(console)console.log("nickdebug2",data)
+        //         liveStats = data
+        //     }
+        // }).fail(function() {
+        //         if(console)console.log( "error" );
         // });
-        // $.get("https://www80.myfantasyleague.com/2022/export?TYPE=liveScoring&L=58663&W="+i+"&JSON=1",function( data ) {
-        //     if(console)console.log("nickdebug3",data)
-        //     liveScoring = data
-        // },"json").fail(function() {
-        //     if(console)console.log( "error" );
+        // $.ajax({
+        //     async: false,
+        //     url: "https://www80.myfantasyleague.com/2022/export?TYPE=liveScoring&L=58663&W="+i+"&JSON=1",
+        //     dataType: 'json',
+        //     success: function( data ) {
+        //         // if(console)console.log("nickdebug3",data)
+        //         liveScoring = data
+        //     }
+        // }).fail(function() {
+        //         if(console)console.log( "error" );
         // });
-        console.log("nickdebug",liveScoring,liveStats);
+        switch(i) {
+            case 3:
+                week3(liveScoring,franchiseDatabase);
+            case 4: 
+                week4(getLiveScoring(i),getLiveStats(formattedWeek),franchiseDatabase,playerDatabase);
+            default:
+                if(console)console.log("well this isn't good")
+        }
+        // console.log("nickdebug",liveScoring,liveStats);
     }
 // console.log($.get("https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_idp_08.txt?RANDOM="+ms));
 });
 
+function getLiveStats(formattedWeek) {
+    var liveStats;
+    const d = new Date();
+    let ms = d.getMilliseconds();
+    $.ajax({
+        async: false, 
+        url: "https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_"+formattedWeek+".txt?RANDOM="+ms,
+        dataType: 'text',
+        success: function( data ) {
+            // if(console)console.log("nickdebug2",data)
+            liveStats = data
+        }
+    }).fail(function() {
+            if(console)console.log( "error" );
+    });
+    return liveStats;
+}
+
+function getLiveScoring(week) {
+    var liveScoring;
+    $.ajax({
+        async: false,
+        url: "https://www80.myfantasyleague.com/2022/export?TYPE=liveScoring&L=58663&W="+week+"&JSON=1",
+        dataType: 'json',
+        success: function( data ) {
+            // if(console)console.log("nickdebug3",data)
+            liveScoring = data
+        }
+    }).fail(function() {
+            if(console)console.log( "error" );
+    });
+    return liveScoring
+}
 /*
 Week 3 = Most Team Points (Matty - 147.48)
 Week 4 = Most All-Purpose Yards
@@ -65,6 +94,10 @@ Week 14 = Player with Most Yards (No QB)
 Week 15 = Most Team Points (Non-playoff Team)
 Week 16 = Most Team Points (Non-playoff Team)
 */
-function week3() {
+function week3(liveScoring,franchises) {
+    console.log(liveScoring.liveScoring,franchises)
+}
+
+function week4(liveScoring,liveStats,franchises,players) {
 
 }
