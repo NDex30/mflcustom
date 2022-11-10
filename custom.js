d7e5e90951fee281a9b99531c44bc0882e3ead89
Ndex30 <oudex30@gmail.com>
2022-11-09 22:20:44 -0600

    cleanup

--- custom.js
+++ custom.js
@@ -1,39 +1,28 @@
 $(function() {
-//  console.log(localStorage.getItem("cache_weeklyResults-3_2022_58663_1665792000"));
 //  console.log(franchiseDatabase);
     console.log("current week",real_ls_week);
     var content = "<table>";
-    //starts at week3
+    /*
+        Week 3 = Most Team Points (Matty - 147.48)
+        Week 4 = Most All-Purpose Yards
+        Week 5 = Most Kicker Points
+        Week 6 = Longest QB TD
+        Week 7 = Most Team Receptions
+        Week 8 = Most DEF Points
+        Week 9 = Most Single Player Receptions
+        Week 10 = Most Points from Single Tight End
+        Week 11 = Most Team TD's
+        Week 12 = Player with Most Points (No QB)
+        Week 13 = Greatest Win Margin
+        Week 14 = Player with Most Yards (No QB)
+        Week 15 = Most Team Points (Non-playoff Team)
+        Week 16 = Most Team Points (Non-playoff Team)
+    */
     for(i = 3;i < real_ls_week;i++) {
         let formattedWeek = i.toLocaleString('en-US', {
             minimumIntegerDigits: 2,
             useGrouping: false
         })
-        // var liveScoring,liveStats;
-        // const d = new Date();
-        // let ms = d.getMilliseconds();
-        // $.ajax({
-        //     async: false, 
-        //     url: "https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_"+formattedWeek+".txt?RANDOM="+ms,
-        //     dataType: 'text',
-        //     success: function( data ) {
-        //         // if(console)console.log("nickdebug2",data)
-        //         liveStats = data
-        //     }
-        // }).fail(function() {
-        //         if(console)console.log( "error" );
-        // });
-        // $.ajax({
-        //     async: false,
-        //     url: "https://www80.myfantasyleague.com/2022/export?TYPE=liveScoring&L=58663&W="+i+"&JSON=1",
-        //     dataType: 'json',
-        //     success: function( data ) {
-        //         // if(console)console.log("nickdebug3",data)
-        //         liveScoring = data
-        //     }
-        // }).fail(function() {
-        //         if(console)console.log( "error" );
-        // });
         switch(i) {
             case 3:
                 var maxPointsFranchise = mostTeamPoints(getLiveScoring(i),franchiseDatabase);
@@ -49,10 +38,8 @@ $(function() {
             default:
                 if(console)console.log("well this isn't good")
         }
-        // console.log("nickdebug",liveScoring,liveStats);
     }
     $('#weekly-challenges-box').append(content);
-// console.log($.get("https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_idp_08.txt?RANDOM="+ms));
 });
 
 function getLiveStats(formattedWeek) {
@@ -64,7 +51,6 @@ function getLiveStats(formattedWeek) {
         url: "https://www80.myfantasyleague.com/fflnetdynamic2022/live_stats_"+formattedWeek+".txt?RANDOM="+ms,
         dataType: 'text',
         success: function( data ) {
-            // if(console)console.log("nickdebug2",data)
             liveStats = data
         }
     }).fail(function() {
@@ -88,34 +74,15 @@ function getLiveScoring(week) {
     });
     return liveScoring
 }
-/*
-Week 3 = Most Team Points (Matty - 147.48)
-Week 4 = Most All-Purpose Yards
-Week 5 = Most Kicker Points
-Week 6 = Longest QB TD
-Week 7 = Most Team Receptions
-Week 8 = Most DEF Points
-Week 9 = Most Single Player Receptions
-Week 10 = Most Points from Single Tight End
-Week 11 = Most Team TD's
-Week 12 = Player with Most Points (No QB)
-Week 13 = Greatest Win Margin
-Week 14 = Player with Most Yards (No QB)
-Week 15 = Most Team Points (Non-playoff Team)
-Week 16 = Most Team Points (Non-playoff Team)
-*/
+
 function mostTeamPoints(liveScoring,franchises) {
     const storageKey = "smashBrosMostTeamPoints";
     var maxScoreFranchise;
     if (localStorage.getItem(storageKey) !== null) {
         maxScoreFranchise = JSON.parse(localStorage.getItem(storageKey));
-        console.log(maxScoreFranchise);
         return maxScoreFranchise;
     }
-    
-    console.log(franchises,maxScoreFranchise);
     for(x in liveScoring.liveScoring.matchup) {
-        //console.log(liveScoring.liveScoring.matchup[x])
         for(y in liveScoring.liveScoring.matchup[x].franchise){
             if(maxScoreFranchise === undefined || parseFloat(liveScoring.liveScoring.matchup[x].franchise[y].score) > parseFloat(maxScoreFranchise.score)){
                 maxScoreFranchise = liveScoring.liveScoring.matchup[x].franchise[y]
@@ -138,26 +105,18 @@ function week4(liveScoring,liveStats,franchises,players) {
 function mostKickerPoints(liveScoring,franchises,players) {
     const storageKey = "smashBrosMostKickerPoints";
     var mostKickerPoints;
-    // console.log("franchises",franchises);
-    // console.log("players",players)
     if (localStorage.getItem(storageKey) !== null) {
         mostKickerPoints = JSON.parse(localStorage.getItem(storageKey));
-        console.log(mostKickerPoints);
         return mostKickerPoints;
     }
     for(x in liveScoring.liveScoring.matchup) {
         for(y in liveScoring.liveScoring.matchup[x].franchise){
-            // console.log(liveScoring.liveScoring.matchup[x].franchise[y])
             for(z in liveScoring.liveScoring.matchup[x].franchise[y].players) {
                 for(zz in liveScoring.liveScoring.matchup[x].franchise[y].players[z]){
-                    // console.log("player",liveScoring.liveScoring.matchup[x].franchise[y].players[z][zz])
                     var playerScore = liveScoring.liveScoring.matchup[x].franchise[y].players[z][zz];
                     var playerInfo = players['pid_'+playerScore.id]
-                    // console.log(playerInfo)
                     if(playerInfo.position === "PK"){
-                        console.log("playerScore",playerScore,"playerInfo",playerInfo,"mostKickerPoints",mostKickerPoints)
                         if(mostKickerPoints === undefined || parseFloat(playerScore.score)  > parseFloat(mostKickerPoints.score)){
-                            // if(console)console.log("pointsCompare",playerScore.score  > mostKickerPoints.score)
                             var playerName = playerInfo.name
                             var franchiseName = franchises["fid_"+liveScoring.liveScoring.matchup[x].franchise[y].id].name
                             mostKickerPoints = {
@@ -165,7 +124,6 @@ function mostKickerPoints(liveScoring,franchises,players) {
                                 ...playerScore,
                                 franchiseName,
                             };
-                            console.log("updated kicker score",mostKickerPoints)
                         }
                     }
                 };
