087329be1ed4fa6773568ecf969d04ae64479ec6
Ndex30 <oudex30@gmail.com>
2022-11-24 08:59:01 -0600

    start new scoring script

---
+++ scoring.js
@@ -0,0 +1,28 @@
+$(function () {
+  let liveScoring = getLiveScoring(real_ls_week);
+  console.log(liveScoring);
+});
+
+function getLiveScoring(week) {
+  var liveScoring;
+  $.ajax({
+    async: false,
+    url:
+      "https://" +
+      window.location.host +
+      "/" +
+      year +
+      "/export?TYPE=liveScoring&L=" +
+      league_id +
+      "&W=" +
+      week +
+      "&JSON=1",
+    dataType: "json",
+    success: function (data) {
+      liveScoring = data;
+    },
+  }).fail(function () {
+    if (console) console.log("error");
+  });
+  return liveScoring;
+}
