d8ddd19e3256679221de336d99f328a884b7cc53
Ndex30 <oudex30@gmail.com>
2022-11-24 09:35:23 -0600

    add franchise info

--- scoring.js
+++ scoring.js
@@ -8,6 +8,21 @@ $(function () {
     for (f in liveScoring.liveScoring.matchup[m].franchise) {
       let franchise = liveScoring.liveScoring.matchup[m].franchise[f];
       let franchiseInfo = franchiseDatabase["fid_" + franchise.id];
+      let franchiseBox = $('<div id="franchise_' + franchise.id + '"></div>');
+      franchiseBox.append(
+        '<div class="franchise-icon"><img src="' +
+          franchiseInfo.icon +
+          '" alt="' +
+          franchiseInfo.abbrev +
+          '" /></div>'
+      );
+      franchiseBox.append(
+        '<div class="franchise-name"><h2>' + franchiseInfo.name + "</h2></div>"
+      );
+      franchiseBox.append(
+        '<div class="franchise-record">' + franchiseInfo.record + "</div>"
+      );
+      matchupBox.append(franchiseBox);
       console.log("franchise", franchiseInfo);
     }
   }
