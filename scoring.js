c56db9d74ec0679f0292a86a659562d5c7472ca5
Ndex30 <oudex30@gmail.com>
2022-11-24 15:54:54 -0600

    add swipe detection

--- scoring.js
+++ scoring.js
@@ -1,3 +1,50 @@
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* right swipe */
+    } else {
+      /* left swipe */
+      alert("swipe left");
+    }
+  } else {
+    if (yDiff > 0) {
+      /* down swipe */
+    } else {
+      /* up swipe */
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
+
 $(function () {
   let liveScoring = getLiveScoring(real_ls_week);
   let projectedScores = getProjectedScore(real_ls_week, year, league_id);
@@ -114,6 +161,9 @@ $(function () {
     }
   }
 
+  var group = document.querySelector("#dexscoring");
+  group.addEventListener("touchstart", handleTouchStart, false);
+  group.addEventListener("touchmove", handleTouchMove, false);
   //   $("#dexscoring").slick({
   //     dots: true,
   //     infinite: false,
