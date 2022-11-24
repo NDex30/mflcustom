6ffd120c8e7626c639e7bad2127ffa3e91350a6e
Ndex30 <oudex30@gmail.com>
2022-11-24 09:09:07 -0600

    don't dupe code

--- scoring.js
+++ scoring.js
@@ -3,26 +3,26 @@ $(function () {
   console.log("livescoring", liveScoring);
 });
 
-function getLiveScoring(week) {
-  var liveScoring;
-  $.ajax({
-    async: false,
-    url:
-      "https://" +
-      window.location.host +
-      "/" +
-      year +
-      "/export?TYPE=liveScoring&L=" +
-      league_id +
-      "&W=" +
-      week +
-      "&JSON=1",
-    dataType: "json",
-    success: function (data) {
-      liveScoring = data;
-    },
-  }).fail(function () {
-    if (console) console.log("error");
-  });
-  return liveScoring;
-}
+// function getLiveScoring(week) {
+//   var liveScoring;
+//   $.ajax({
+//     async: false,
+//     url:
+//       "https://" +
+//       window.location.host +
+//       "/" +
+//       year +
+//       "/export?TYPE=liveScoring&L=" +
+//       league_id +
+//       "&W=" +
+//       week +
+//       "&JSON=1",
+//     dataType: "json",
+//     success: function (data) {
+//       liveScoring = data;
+//     },
+//   }).fail(function () {
+//     if (console) console.log("error");
+//   });
+//   return liveScoring;
+// }
